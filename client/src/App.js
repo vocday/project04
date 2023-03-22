5717a69e73b5bbcc3893dfa1cf33ef0d0874a094
Dat266 <datdev266@gmail.com>
2023-03-22 21:55:05 +0700

    structure client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StagesPage from "./pages/StagesPage/StagesPage";
+
+function App() {
+	return (
+		<div className="App">
+			<StagesPage />
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/pages/StagesPage/StagesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StagesPage = () => {
+	return <div>StagesPage</div>;
+};
+
+export default StagesPage;
