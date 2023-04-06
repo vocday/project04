52460e70e8d323f785d1bd5377f67e477abf7e51
Quy <quy4312@gmail.com>
2023-04-07 00:33:04 +0700

    fix breadcumb

--- client/src/components/Comment/Comment.jsx
+++ client/src/components/Comment/Comment.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import classNames from "classnames/bind";
-import { Avatar, Button, Input, Space } from "antd";
+import { Avatar, Button, Input, Space, Divider } from "antd";
 
 import styles from "./Comment.module.scss";
 
 const cx = classNames.bind(styles);
 
 const Comment = () => {
-	return (
-		<div className={cx("comment")}>
-			<h4>Đánh giá : </h4>
-			<div className={cx("comment--display")}>
-				<Space size={10}>
-					<Avatar>Đ</Avatar>
-					<p>Chúng tôi đang combat rồi !</p>
-				</Space>
+  return (
+    <>
+      <Divider />
+      <div className={cx("comment")}>
+        <h4>Đánh giá : </h4>
+        <div className={cx("comment--display")}>
+          <Space size={10}>
+            <Avatar>Đ</Avatar>
+            <p>Chúng tôi đang combat rồi !</p>
+          </Space>
 
-				<Space size={10}>
-					<Avatar>B</Avatar>
-					<p>Chúng tôi đang combat rồi !</p>
-				</Space>
-			</div>
+          <Space size={10}>
+            <Avatar>B</Avatar>
+            <p>Chúng tôi đang combat rồi !</p>
+          </Space>
+        </div>
 
-			<div className={cx("comment--action")}>
-				<Space>
-					<Avatar>Đ</Avatar>
-					<Input style={{ width: 700 }} />
-					<Button type="primary"> Đánh giá</Button>
-				</Space>
-			</div>
-		</div>
-	);
+        <div className={cx("comment--action")}>
+          <Space>
+            <Avatar>Đ</Avatar>
+            <Input style={{ width: 700 }} />
+            <Button type="primary"> Đánh giá</Button>
+          </Space>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Comment;
